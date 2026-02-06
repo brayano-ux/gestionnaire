4d8f6b7022c91ee72cd455a644c4702e01d23b87
Developer <dev@example.com>
2026-02-06 21:28:15 +0100

    Simplify vite config - remove custom rollup for Service Worker

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-        sw: 'public/sw.ts'
-      },
-      output: {
-        entryFileNames: '[name].js'
-      }
-    }
-  }
 })
